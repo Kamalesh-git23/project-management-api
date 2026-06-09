dd9a3afddff5faf47992a82de0b14f65be9dfc05
Kamalesh-git23 <kamalesh23112005@gmail.com>
2026-06-09 18:24:51 +0530

    Initial project setup

---
+++ src/server.js
@@ -0,0 +1,38 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import prisma from "./config/prisma.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "Project Management API Running"
+    });
+});
+
+async function testDB(){
+
+    try{
+        await prisma.$connect();
+        console.log("Database Connected");
+    }catch(error){
+        console.error(" Database Connection Failed");
+        console.error(error);        
+    }
+}
+
+
+testDB();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:5000`);
+});
+
